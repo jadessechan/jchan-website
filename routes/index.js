67313682e8d999152b697b5e6a7e19e3cbf191b1
Jadesse Chan <jdlychan@gmail.com>
2020-10-06 23:54:38 -0500

    test alert message (script tag)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  alert('This website is still under development. Keep coming back for updates!');
   res.render('index');
 });
 
