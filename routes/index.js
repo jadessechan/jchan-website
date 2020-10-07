c01744d89b2af24b2d55109f7e5db0b37f4db2bb
Jadesse Chan <jdlychan@gmail.com>
2020-10-06 23:43:53 -0500

    added in construction alert message

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index');
+  alert('This website is still under development. Keep coming back for updates!');
 });
 
 module.exports = router;
